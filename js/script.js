ea14ac1b7e78bab3a4374cdbe0fff4c2024b56f0
Shannqa <116514532+Shannqa@users.noreply.github.com>
2022-12-04 22:44:32 +0100

    Add erase button

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 const sketchpad = document.querySelector('.sketchpad');
+const buttonErase = document.querySelector('.button-erase');
+const buttonColor = document.querySelector('.button-color');
 
 let mouseDown;
 let boxColor = 'black';
+let erase = false;
+
+buttonErase.addEventListener('click', () => {
+  if (erase === false) {
+    buttonErase.classList.add('button-active');
+    boxColor = 'white';
+    erase = true;
+  } else {
+    buttonErase.classList.remove('button-active');
+    boxColor = 'black';
+    erase = false;
+  }
+  
+});
+
 
 function createBoxes(number) {
   for (let j = 0; j < number; j++) {
