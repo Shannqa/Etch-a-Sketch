fb4cc3155eefebc12b9f3dadfadbf0ee40e5efaa
Shannqa <116514532+Shannqa@users.noreply.github.com>
2022-12-04 21:30:57 +0100

    Add the ability to draw on the sketchpad

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const sketchpad = document.querySelector('.sketchpad');
 
+let mouseDown;
 
 function createBoxes(number) {
   for (let j = 0; j < number; j++) {
@@ -8,11 +9,25 @@ function createBoxes(number) {
     for (let i = 0; i < number; i++) {
       let box = document.createElement('div')
       box.classList.add('box');
+      box.addEventListener('mousedown', () => {
+        mouseDown = true;
+      });
+      box.addEventListener('mouseup', () => {
+        mouseDown = false;
+      });
+      box.addEventListener('mousemove', () => {
+        if (mouseDown === true) {
+          box.style.backgroundColor = "green";
+        } else if (mouseDown === false ) {
+          return;
+        }
+      })
       line.appendChild(box);
     }
     sketchpad.appendChild(line);
 
   }
 }
+
 
 createBoxes(16);
